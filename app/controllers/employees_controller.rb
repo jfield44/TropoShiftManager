a5c72f047c4fc9039f5f023e9e35eb30bc7afc07
Jonathan Field <jonfield@me.com>
2016-01-17 18:58:36 +0000

    Initial Commit

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -66,7 +66,7 @@ class EmployeesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_employee
-      @employee = Employee.find(params[:id])
+      @employee = Employee.find_by_employee_number(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
